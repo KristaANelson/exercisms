a859efec4622fb7e18b906461d9ef14ea8ef05f8
Krista Nelson <krista@glassbreakers.co>
2016-11-22 15:46:51 -0600

    version 2 for panagram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,6 +1,14 @@
 class Pangram
-  def self.is_pangram?(string)
-    string.downcase.scan(/[a-z]/).uniq.length == 26
+  def self.is_pangram?(str)
+    (required_chars - included_chars(str)).empty?
+  end
+
+  def self.required_chars
+    ("a".."z").to_a
+  end
+
+  def self.included_chars(str)
+    str.downcase.chars
   end
 end
 
